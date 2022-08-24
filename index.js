735b8019ca363dc80f9f49b338c37b131c408c39
Laura <laur730t@stud.kea.dk>
2022-08-24 11:08:22 +0200

    øvelse 2A

---
+++ index.js
@@ -0,0 +1,13 @@
+//Laver konstanter
+const overskrift = document.querySelector("h1");
+console.log(overskrift);
+const underoverskrift = document.querySelector("h2");
+console.log(underoverskrift);
+const foto = document.querySelector("img");
+console.log(foto);
+const lilleo = document.querySelector("h3");
+console.log(lilleo);
+const tekst = document.querySelector("p");
+console.log(tekst);
+const footer = document.querySelector("footer");
+console.log(footer);
